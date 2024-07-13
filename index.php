b4f1c2ac7fa44e7ee04f982237a7a658230894da
RaihanaFerdousi <raihanaferdousi110@gmail.com>
2024-07-13 21:18:19 +0600

    initial comment

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+// echo " I love Bangladesh";
+
+/*
+
+fdajkdlfjadsf
+dsfasdflsdkjf;s
+dsfjsadfkld;sfjsd
+dsfdsfsdfsd
+
+
+
+*/ 
+
+
+
+echo "We are in a class";
+
+// echo " dfjads fkasdjf";
